8786fa56e8489f601cd4aaeb772de623e6ddafcc
Andrew Thorp <andrew.thorp.dev@gmail.com>
2020-11-11 17:17:40 -0500

    Add build script

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+use std::time::SystemTime;
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+fn generate_random_vector(n: isize) -> Vec<isize> {
+    let mut vec: Vec<isize> = (0..n).collect();
+    vec.shuffle(&mut thread_rng());
+    vec
+}
+
+pub fn sort(list: Vec<isize>) -> Vec<isize> {
+    mergesort(list)
+}
+
+
+/// Sort a vector using bubble sort
+#[warn(dead_code)]
+fn bubblesort<T: Ord>(mut list: Vec<T>) -> Vec<T>{
+    Vec::new()
+}
+
+/// Sort a vector using merge sort
+#[warn(dead_code)]
+fn mergesort<T: Ord + Clone + Copy>(mut list: Vec<T>) -> Vec<T>{
+    Vec::new()
+}
+
+fn check_time(n: isize) {
+    let vec: Vec<isize> = generate_random_vector(n);
+
+    let sys_time = SystemTime::now();
+    sort(vec.clone());
+    let difference = sys_time.elapsed().unwrap();
+
+    println!(
+        "Sorting time on array of size {}: {:?}",
+        vec.len(),
+        difference
+    );
+}
+
+fn main() {
+    let mut vec = generate_random_vector(40);
+    println!("Good day. This is a demonstration of a sorting algorith.");
+    println!("Below is a shuffled list of elements:\n{:?}", vec);
+    vec = sort(vec.clone());
+    println!(
+        "After sorting, you can clearly see the list is now ordered:\n{:?}",
+        vec
+    );
+
+    for i in 1..10 {
+        check_time(i * 500)
+    }
+}
+
+
+
+
+
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[quickcheck]
+    fn sorted_output_is_sorted(xs: Vec<isize>) -> bool {
+        if xs.len() <= 1 {
+            return true;
+        }
+        let result = sort(xs.clone());
+
+        for i in 0..(result.len() - 2) {
+            if result[i] > result[i + 1] {
+                println!("Found error at index {}: {} > {}", i, result[i], result[i + 1]);
+                return false;
+            }
+        }
+        true
+    }
+
+    #[quickcheck]
+    fn check_time(xs: Vec<isize>) -> bool {
+        let sys_time = SystemTime::now();
+        sort(xs.clone());
+        let difference = sys_time
+            .duration_since(sys_time)
+            .expect("Clock may have gone backwards");
+        println!(
+            "Sorting time on array of size {}: {:?}",
+            xs.len(),
+            difference
+        );
+        return true;
+    }
+}
---
+++ src/sort_tests.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+#[cfg(test)]
+mod tests {
+    fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
+        let mut rev = vec!();
+        for x in xs {
+            rev.insert(0, x.clone())
+        }
+        rev
+    }
+
+    #[quickcheck]
+    fn double_reversal_is_identity(xs: &mut [isize]) -> bool {
+        println!("testing!");
+        xs == reverse(&reverse(&xs))
+    }
+}
